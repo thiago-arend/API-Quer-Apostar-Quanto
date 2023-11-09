88bf55fcbb6d21bd7487f7e10952236bfc7d379b
thiago-arend <thiago.arend@gmail.com>
2023-11-09 14:51:26 -0300

    chore: initial api configuration

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { json, Request, Response } from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.get("/health", (req: Request, res: Response) => res.send("ok!"));
+
+export default app;
